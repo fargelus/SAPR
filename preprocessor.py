ed9948e9875c6b7e65cac20675d663e57833118d
fargelus <shmurnov@list.ru>
2015-11-09 07:35:21 +0300

    Обновлен внешний вид препроцессора

--- preprocessor.py
+++ preprocessor.py
@@ -1,66 +1,25 @@
 __author__ = 'dima'
 
 from tkinter import *
+from tkinter.filedialog import askopenfilename, asksaveasfilename
+from tkinter.messagebox import showinfo
 
 
 class PreprocessorWin(Frame):
     def __init__(self, parent=None):
-        Frame.__init__(self, parent, width=800, height=600)
-        self.pack()
+        Frame.__init__(self, parent)
+        self.pack(expand=YES, fill=BOTH)
+
         self.master.title('Окно препроцессора')
 
         self.make_widgets()
 
     def make_widgets(self):
-        self.top_frame = Frame(self, width=640, height=480)
-        self.top_frame.grid(row=0, column=0)
+        Innerframe(self).pack(side=BOTTOM)
+        Construction(self).pack(side=LEFT)
+        Drawbutton(self).pack(side=RIGHT)
 
-        self.bottom_frame = Frame(self, width=640, height=120)
-        self.bottom_frame.grid(row=1, column=0)
 
-        self.right_frame = Frame(self, width=160, height=600)
-        self.right_frame.grid(row=0, column=1, rowspan=2)
-
-        self.construction = Canvas(self.top_frame, width=640, height=480, bg='white')
-        self.construction.pack(fill=BOTH)
-
-        self.draw_btn = Button(self.right_frame, text='О\n'
-                                                      'Т\n'
-                                                      'Р\n'
-                                                      'И\n'
-                                                      'С\n'
-                                                      'О\n'
-                                                      'В\n'
-                                                      'А\n'
-                                                      'Т\n'
-                                                      'Ь\n',
-                                font=('Verdana', 14, 'italic bold'),
-                                command=self.draw)
-
-        self.draw_btn.pack(side=TOP, expand=YES, fill=BOTH)
-
-        self.open_btn = Button(self.bottom_frame, text='Открыть файл', font=('Verdana', 10, 'italic bold'),
-                               command=self.open_file)
-        self.open_btn.pack(side=LEFT, expand=YES, fill=BOTH)
-
-        self.save_btn = Button(self.bottom_frame, text='Сохранить в файл', font=('Verdana', 10, 'italic bold'),
-                               command=self.save_file)
-        self.save_btn.pack(side=LEFT, expand=YES, fill=BOTH)
-
-        self.param_btn = Button(self.bottom_frame, text='Параметры конструкции', font=('Verdana', 10, 'italic bold'),
-                               command=self.read_parameters)
-        self.param_btn.pack(side=LEFT, expand=YES, fill=BOTH)
-
-        self.force_btn = Button(self.bottom_frame, text='Нагрузки', font=('Verdana', 10, 'italic bold'),
-                               command=self.read_forces)
-        self.force_btn.pack(side=LEFT, expand=YES, fill=BOTH)
-
-        self.about_btn = Button(self.bottom_frame, text='О программе', font=('Verdana', 10, 'italic bold'),
-                               command=self.about_win)
-        self.about_btn.pack(side=LEFT, expand=YES, fill=BOTH)
-
-    def draw(self):
-        pass
 
     def open_file(self):
         pass
@@ -76,3 +35,81 @@ class PreprocessorWin(Frame):
 
     def about_win(self):
         pass
+
+
+class Drawbutton(Frame):
+    def __init__(self, parent=None):
+        Frame.__init__(self, parent)
+        self.pack(fill=BOTH, expand=YES)
+
+        self.btn = Button(self, text='О\n'
+                          'Т\n'
+                          'Р\n'
+                          'И\n'
+                          'С\n'
+                          'О\n'
+                          'В\n'
+                          'А\n'
+                          'Т\n'
+                          'Ь\n',
+                      font=('Times', 14, 'italic bold')).pack(expand=YES, fill=BOTH)
+
+        #self.master.config(width=160, height=600)
+
+
+class Innerframe(Frame):
+    def __init__(self, parent=None):
+        Frame.__init__(self, parent, width=640, height=120)
+        self.pack(expand=YES, fill=BOTH)
+
+        self.make_widgets()
+
+    def make_widgets(self):
+        self.opn_file_btn = Button(self, text='Открыть файл', font=('Times', 10, 'italic bold'),
+                                command=self.open_file)
+        self.opn_file_btn.pack(side=LEFT, expand=YES, fill=BOTH)
+
+        self.save_file_btn = Button(self, text='Сохранить в файл', font=('Times', 10, 'italic bold'),
+                                    command=self.save_file)
+        self.save_file_btn.pack(side=LEFT, expand=YES, fill=BOTH)
+
+        self.param_btn = Button(self, text='Параметры конструкции', font=('Times', 10, 'italic bold'))
+                               # command=self.open_param_win)
+        self.param_btn.pack(side=LEFT, expand=YES, fill=BOTH)
+
+        self.force_btn = Button(self, text='Нагрузки', font=('Times', 10, 'italic bold'))
+                               # command=self.open_force_win)
+        self.force_btn.pack(side=LEFT, expand=YES, fill=BOTH)
+
+        self.about_btn = Button(self, text='О программе', font=('Times', 10, 'italic bold'),
+                                command=self.about)
+        self.about_btn.pack(side=LEFT, expand=YES, fill=BOTH)
+
+    def open_file(self):
+        filename = askopenfilename(parent=self, defaultextension='.db', filetypes=[('Database', '.db'),
+                                                                                   ('SQLite3', '.sqlite3'),
+                                                                                   ('SQLite', '.sqlite')],
+                                   initialdir='/home/dima/Рабочий стол/САПР/Computer Mechanic/data')
+        print(filename)
+
+    def save_file(self):
+        filename = asksaveasfilename(parent=self, defaultextension='.db', filetypes=[('Database', '.db'),
+                                                                                   ('SQLite3', '.sqlite3'),
+                                                                                   ('SQLite', '.sqlite')],
+                                   initialdir='/home/dima/Рабочий стол/САПР/Computer Mechanic/data')
+        print(filename)
+
+
+    def about(self):
+        showinfo('Справка', '')
+
+
+class Construction(Frame):
+    def __init__(self, parent=None):
+        Frame.__init__(self, parent)
+        self.pack()
+
+        self.cv = Canvas(self, width=640, height=480, bg='white')
+        self.cv.pack(expand=YES, fill=BOTH)
+
+        self.cv.create_text(300, 25, text='Конструкция', fill='gray', font=('Times', 15, 'italic bold'))
